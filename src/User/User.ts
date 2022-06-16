5cd7af972c9d8fe1752db82fc42967e854d0ea20
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2022-06-16 23:11:37 +0530

    Renamed: /UserManager to /User Folder

---
+++ src/User/User.ts
@@ -0,0 +1,15 @@
+export class User {
+
+  private _userId: TUserId;
+  private _userInfo: TUserInfo;
+  
+  constructor ( userId: TUserId, userInfo: TUserInfo ) {
+    this._userId = userId;
+    this._userInfo = userInfo;
+  }
+
+  get userId () { return this._userId; }
+  get userInfo () { return this._userInfo;  }
+  set userInfo ( userInfo: TUserInfo ) { this._userInfo = userInfo; }
+
+}
