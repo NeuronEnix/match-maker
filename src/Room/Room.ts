2075336209e631e08c9cde88b664c4473c25e0f4
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2022-06-18 11:08:34 +0530

    Init Commit

---
+++ src/Room/Room.ts
@@ -0,0 +1,94 @@
+import { Group } from "../Group/Group";
+
+let roomId: TRoomId = 1;
+
+export class Room {
+
+  private readonly _roomId: TRoomId;
+  private readonly _config: TRoomConfig;
+  private readonly _groupList: Group[];
+  private readonly _userIdMapToGroup: Map<TUserId,Group>;
+
+  constructor ( maxGroup: number, maxUserPerGroup: number, spreadUserAcrossGroup: boolean  ) {
+
+    this._roomId = roomId++;
+    this._config = { maxGroup, maxUserPerGroup, spreadUserAcrossGroup };
+
+    this._groupList = [];
+    for ( let i=0; i<this._config.maxGroup; i++ )
+      this._groupList.push( new Group( this._config.maxUserPerGroup ) );
+    
+    this._userIdMapToGroup = new Map<TUserId,Group>();
+  }
+  
+  get roomId() : TRoomId {
+    return this._roomId;
+  }
+  get userCount() : number {
+    return this._userIdMapToGroup.size;
+  }
+  get groupCount() : number {
+    return this._groupList.length;
+  }
+  private _addGroupBySpread_noCheck( groupToBeAdded: Group ) : void {
+
+    // add users from group by spreading across groups available in room whichever has vacancy
+    const userItr = groupToBeAdded.getUserIterator();
+    for ( const roomGroup of this._groupList ) {
+      for ( let i=0; i<roomGroup.hasVacancy(); ++i ) {
+        const { value: user, done } = userItr.next();
+        if ( done ) return;
+        this._userIdMapToGroup.set( user.userId, roomGroup );
+        roomGroup.addUser( user );
+      }
+    }
+
+  }
+
+  private _addGroupRestrictedToOneRoomGroup_noCheck( groupToBeAdded: Group ) : void {
+   
+    // add users from 'groupToBeAdded' to any one single groupHeldBy room 
+    for ( const roomGroup of this._groupList ) {
+      if ( !roomGroup.hasVacancy( groupToBeAdded.userCount ) ) continue;
+      groupToBeAdded.forEachUser( user => {
+        this._userIdMapToGroup.set( user.userId, roomGroup );
+        roomGroup.addUser( user );
+      })
+    }
+  }
+  
+  addGroup( groupToBeAdded: Group ) : boolean {
+
+    // Param check
+    if ( !this.hasVacancy( groupToBeAdded.userCount ) ) return false;
+
+    if ( this._config.spreadUserAcrossGroup )
+      this._addGroupBySpread_noCheck( groupToBeAdded );
+    else this._addGroupRestrictedToOneRoomGroup_noCheck( groupToBeAdded );
+
+    return true;
+    
+  }
+
+  hasVacancy( count: number ) : boolean {
+    if ( this._config.spreadUserAcrossGroup )
+      return (this._config.maxGroup * this._config.maxUserPerGroup - this._userIdMapToGroup.size ) >= count;
+
+    for ( const group of this._groupList )
+      if ( group.hasVacancy( count ) ) return true;
+
+    return false;
+  }
+    
+  delUser( group: Group ) : void {
+    group.forEachUser( user => {
+      const userInRoomGroup = this._userIdMapToGroup.get( user.userId );
+      if ( userInRoomGroup ) {
+        userInRoomGroup.delUser( user );
+        this._userIdMapToGroup.delete( user.userId );
+      }
+    })
+  }
+
+}
+
---
+++ src/Room/RoomManger.ts
@@ -0,0 +1,47 @@
+import { Group } from "../Group/Group";
+import { Room } from "./Room";
+
+
+export class RoomManager {
+
+  private _roomMap: Map< TRoomId, Room >;
+  private _config: TRoomConfig;
+
+  constructor ( maxGroup: number, maxUserPerGroup: number, spreadUserAcrossGroup: boolean  ) {
+    
+    this._roomMap = new Map< TRoomId, Room >();
+
+    this._config = { maxGroup, maxUserPerGroup, spreadUserAcrossGroup };
+    
+  }
+
+  addGroup( groupToBeAdded: Group ) : Room {
+    const roomItr = this._roomMap.values();
+
+    // try to add to existing room
+    while ( true ) {
+      const { value: room, done } = roomItr.next();
+      if( done ) break;
+      if ( !room.hasVacancy( groupToBeAdded.userCount ) ) continue;
+      room.addGroup( groupToBeAdded );
+      return room;
+    }
+
+    // will come here if we cannot add group to room
+    // therefore create a new room and add the group to it
+    const newRoom = new Room( this._config.maxGroup, this._config.maxUserPerGroup, this._config.spreadUserAcrossGroup );
+    newRoom.addGroup( groupToBeAdded );
+    return newRoom;
+
+  }
+  
+  getUser( userId: TRoomId) : Room | undefined {
+    return this._roomMap.get( userId );
+  }
+  
+  delUser( room: Room ) : boolean {
+    return  this._roomMap.delete( room.roomId );
+  }
+
+}
+
